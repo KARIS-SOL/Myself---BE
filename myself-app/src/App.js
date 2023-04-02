b9e79eda2fb486e8781380a7c52f249d740da85d
Karis-Sol <karis.baik1124@gmail.com>
2023-04-02 19:16:18 +0900

    useRef 기본

--- myself-app/src/App.js
+++ myself-app/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
-import Timer from './components/Timer';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const countRef = useRef(0);
+
+  console.log(countRef);
+  console.log('렌더링~~');
+
+  const increaseCountState = () => {
+    setCount(count + 1);
+  };
+
+  const increaseRefState = () => {
+    countRef.current = countRef.current + 1;
+  };
+
   return (
-    <div className="App">
-      <Timer />
+    <div>
+      <p>State: {count}</p>
+      <button onClick={increaseCountState}>State 올려!</button>
+      <br />
+      <p>Ref: {countRef.current}</p>
+      <button onClick={increaseRefState}>Ref 올려!!</button>
     </div>
   );
-}
+};
 
 export default App;
--- myself-app/src/components/Timer.jsx
+++ myself-app/src/components/Timer.jsx
@@ -3,11 +3,13 @@ import {useState, useEffect} from 'react';
 
 export default function Timer() {
 const [showTimer, setShowTimer] = useState(false);
-
+// timer 가 시작 될때 mount 되었을때
 useEffect(() => {
   const timer = setInterval(()=> {
     console.log('타이머 돌아가는중');
   }, 1000);
+
+  // timer 가 화면에서 사라질때 unmount 되었을때
   return () => {
     clearInterval(timer);
     console.log('타이머가 종료되었습니다');
