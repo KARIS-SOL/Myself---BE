6d926af0c6069aa2f175ce1b927f42be3ce7e34f
Karis-Sol <karis.baik1124@gmail.com>
2023-04-02 20:14:00 +0900

    useRef와 Var 의 차이로인한 렌더링 이후 변화

--- myself-app/src/App.js
+++ myself-app/src/App.js
@@ -2,27 +2,37 @@ import React, { useState, useRef } from 'react';
 import './App.css';
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [renderer, setRenderer] = useState(0);
+  // 아무리 렌더링이되도 마운트가 시작해서 마운트가 해제될때까지 같은값을 유지함
   const countRef = useRef(0);
+  // 렌더링 될때마다 초기값 0 으로 돌아감
+  let countVar = 0;
 
-  console.log(countRef);
-  console.log('렌더링~~');
-
-  const increaseCountState = () => {
-    setCount(count + 1);
+  const doRendering = () => {
+    setRenderer(renderer + 1);
   };
 
-  const increaseRefState = () => {
+  const increaseRef = () => {
     countRef.current = countRef.current + 1;
+    console.log('ref:', countRef.current);
   };
 
+  const increaseVar = () => {
+    countVar = countVar + 1;
+    console.log('var:', countVar);
+  };
+
+  const printResults = () => {
+    console.log(`ref: ${countRef.current}, var: ${countVar}`);
+  };
   return (
     <div>
-      <p>State: {count}</p>
-      <button onClick={increaseCountState}>State 올려!</button>
-      <br />
       <p>Ref: {countRef.current}</p>
-      <button onClick={increaseRefState}>Ref 올려!!</button>
+      <p>Var: {countVar}</p>
+      <button onClick={doRendering}>Render!</button>
+      <button onClick={increaseRef}>Ref 올려!</button>
+      <button onClick={increaseVar}>Var 올려!</button>
+      <button onClick={printResults}>Ref, Var 값 출력!</button>
     </div>
   );
 };
