c34d5d9aafa5198da6d522e576b65b534d326ec9
Karis-Sol <karis.baik1124@gmail.com>
2023-04-02 17:03:40 +0900

    useState 연습

---
+++ myself-app/src/App.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+function App() {
+  const [names, setNames] = useState(['김민수', '홍길동']);
+  const [input, setInput] = useState('');
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleUpload = () => {
+    setNames((prevState) => {
+      return [input, ...prevState];
+    });
+  };
+  return (
+    <div>
+      <input type="text" value={input} onChange={handleInputChange} />
+      <button onClick={handleUpload}> Upload</button>
+      {names.map((name, idx) => {
+        return <p key={idx}>{name}</p>;
+      })}
+    </div>
+  );
+}
+
+export default App;
